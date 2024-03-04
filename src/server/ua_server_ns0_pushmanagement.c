c8f84df1467b36e9ce0e088b5a975e2369358e64
Noel Graf <noel.gerome.graf@gmail.com>
2024-03-04 11:36:39 +0100

    Make the addCertificate and removeCertficate functions available in the information model

--- src/server/ua_server_ns0_pushmanagement.c
+++ src/server/ua_server_ns0_pushmanagement.c
@@ -6,8 +6,11 @@
  */
 
 #include "ua_server_internal.h"
+#include <open62541/plugin/log_stdout.h>
 
 #ifdef UA_ENABLE_PUSHMANAGEMENT
+
+#define UA_SHA1_LENGTH 20
 
 static UA_StatusCode
 writeGDSNs0VariableArray(UA_Server *server, const UA_NodeId id, void *v,
@@ -27,6 +30,28 @@ writeGDSNs0Variable(UA_Server *server, const UA_NodeId id,
     UA_Variant_init(&var);
     UA_Variant_setScalar(&var, v, type);
     return writeValueAttribute(server, id, &var);
+}
+
+static UA_Boolean
+compareThumbprint(const UA_String *str1, const UA_String *str2) {
+    if (str1->length != str2->length)
+        return false;
+
+    for (size_t i = 0; i < str1->length; i++) {
+        char ch1 = str1->data[i];
+        char ch2 = str2->data[i];
+
+        // Convert characters to lowercase if they are uppercase
+        if (ch1 >= 'A' && ch1 <= 'Z')
+            ch1 += ('a' - 'A');
+        if (ch2 >= 'A' && ch2 <= 'Z')
+            ch2 += ('a' - 'A');
+
+        // Compare the characters
+        if (ch1 != ch2)
+            return false;
+    }
+    return true;
 }
 
 static UA_StatusCode
@@ -158,6 +183,149 @@ getRejectedList(UA_Server *server,
 }
 
 static UA_StatusCode
+addCertificate(UA_Server *server,
+               const UA_NodeId *sessionId, void *sessionHandle,
+               const UA_NodeId *methodId, void *methodContext,
+               const UA_NodeId *objectId, void *objectContext,
+               size_t inputSize, const UA_Variant *input,
+               size_t outputSize, UA_Variant *output) {
+
+    UA_StatusCode retval = UA_STATUSCODE_GOOD;
+
+    /*check for input types*/
+    if(!UA_Variant_hasScalarType(&input[0], &UA_TYPES[UA_TYPES_BYTESTRING]) || /*Certificate*/
+       !UA_Variant_hasScalarType(&input[1], &UA_TYPES[UA_TYPES_BOOLEAN])) /*IsTrustedCertificate*/
+        return UA_STATUSCODE_BADTYPEMISMATCH;
+
+    UA_ByteString certificate = *(UA_ByteString *)input[0].data;
+    UA_Boolean isTrustedCertificate = *(UA_Boolean *)input[1].data;
+
+    if(!isTrustedCertificate || certificate.length == 0)
+        return UA_STATUSCODE_BADCERTIFICATEINVALID;
+
+    /* TODO: check if TrustList Object is already open */
+    /* TODO: check if TrustList Object is read only */
+
+    UA_TrustListDataType trustList;
+    memset(&trustList, 0, sizeof(UA_TrustListDataType));
+    UA_ByteString certificates[1];
+    certificates[0] = certificate;
+
+    trustList.specifiedLists = UA_TRUSTLISTMASKS_TRUSTEDCERTIFICATES;
+    trustList.trustedCertificates = certificates;
+    trustList.trustedCertificatesSize = 1;
+
+    UA_CertificateGroup certGroup;
+    UA_NodeId defaultApplicationGroup = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTAPPLICATIONGROUP_TRUSTLIST);
+    UA_NodeId defaultUserTokenGroup = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTUSERTOKENGROUP_TRUSTLIST);
+    if(UA_NodeId_equal(objectId, &defaultApplicationGroup)) {
+        certGroup = server->config.secureChannelPKI;
+    }
+    else if(UA_NodeId_equal(objectId, &defaultUserTokenGroup)) {
+        certGroup = server->config.sessionPKI;
+    }
+    else {
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+
+    if(certGroup.verifyCertificate(&certGroup, &certificate, NULL, 0) != UA_STATUSCODE_GOOD) {
+        return UA_STATUSCODE_BADCERTIFICATEINVALID;
+    }
+
+    retval = certGroup.addToTrustList(&certGroup, &trustList);
+    return retval;
+}
+
+static UA_StatusCode
+removeCertificate(UA_Server *server,
+                const UA_NodeId *sessionId, void *sessionHandle,
+                const UA_NodeId *methodId, void *methodContext,
+                const UA_NodeId *objectId, void *objectContext,
+                size_t inputSize, const UA_Variant *input,
+                size_t outputSize, UA_Variant *output) {
+
+    UA_StatusCode retval = UA_STATUSCODE_GOOD;
+
+    /*check for input types*/
+    if(!UA_Variant_hasScalarType(&input[0], &UA_TYPES[UA_TYPES_STRING]) || /*Thumbprint*/
+       !UA_Variant_hasScalarType(&input[1], &UA_TYPES[UA_TYPES_BOOLEAN])) /*IsTrustedCertificate*/
+        return UA_STATUSCODE_BADTYPEMISMATCH;
+
+    UA_String thumbprint = *(UA_String *)input[0].data;
+    UA_Boolean isTrustedCertificate = *(UA_Boolean *)input[1].data;
+
+    /* TODO: check if TrustList Object is already open */
+    /* TODO: check if TrustList Object is read only */
+    /* TODO: If the Certificate is a CA Certificate that has CRLs then all CRLs for that CA are removed as well */
+
+    UA_CertificateGroup certGroup;
+    UA_NodeId defaultApplicationGroup = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTAPPLICATIONGROUP_TRUSTLIST);
+    UA_NodeId defaultUserTokenGroup = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTUSERTOKENGROUP_TRUSTLIST);
+    if(UA_NodeId_equal(objectId, &defaultApplicationGroup)) {
+        certGroup = server->config.secureChannelPKI;
+    }
+    else if(UA_NodeId_equal(objectId, &defaultUserTokenGroup)) {
+        certGroup = server->config.sessionPKI;
+    }
+    else {
+        return UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+
+    UA_TrustListDataType trustList;
+    memset(&trustList, 0, sizeof(UA_TrustListDataType));
+    trustList.specifiedLists = UA_TRUSTLISTMASKS_ALL;
+
+    UA_ByteString *certificates;
+    size_t certificatesSize = 0;
+    certGroup.getTrustList(&certGroup, &trustList);
+
+    if(isTrustedCertificate) {
+        certificates = trustList.trustedCertificates;
+        certificatesSize = trustList.trustedCertificatesSize;
+    } else {
+        certificates = trustList.issuerCertificates;
+        certificatesSize = trustList.issuerCertificatesSize;
+    }
+
+    UA_TrustListDataType list;
+    memset(&list, 0, sizeof(UA_TrustListDataType));
+
+    UA_String thumbpr = UA_STRING_NULL;
+    thumbpr.length = (UA_SHA1_LENGTH * 2);
+    thumbpr.data = (UA_Byte*)malloc(sizeof(UA_Byte)*thumbpr.length);
+    for(size_t i = 0; i < certificatesSize; i++) {
+        UA_CertificateUtils_getThumbprint( &certificates[i], &thumbpr);
+        if(compareThumbprint(&thumbprint, &thumbpr)) {
+            UA_ByteString certificate[1];
+            certificate[0] = certificates[i];
+
+            if(isTrustedCertificate) {
+                list.specifiedLists = UA_TRUSTLISTMASKS_TRUSTEDCERTIFICATES;
+                list.trustedCertificates = certificate;
+                list.trustedCertificatesSize = 1;
+            } else {
+                list.specifiedLists = UA_TRUSTLISTMASKS_ISSUERCERTIFICATES;
+                list.issuerCertificates = certificate;
+                list.issuerCertificatesSize = 1;
+            }
+            break;
+        }
+    }
+
+    if(list.specifiedLists != UA_TRUSTLISTMASKS_NONE) {
+        retval = certGroup.removeFromTrustList(&certGroup, &list);
+    } else {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "The certificate to remove was not found");
+        retval = UA_STATUSCODE_BADINVALIDARGUMENT;
+    }
+
+    UA_String_clear(&thumbpr);
+    UA_TrustListDataType_clear(&trustList);
+
+    return retval;
+}
+
+static UA_StatusCode
 applyChanges(UA_Server *server,
              const UA_NodeId *sessionId, void *sessionHandle,
              const UA_NodeId *methodId, void *methodContext,
@@ -225,6 +393,16 @@ initNS0PushManagement(UA_Server *server) {
 
     retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_APPLYCHANGES), applyChanges);
     retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATIONTYPE_APPLYCHANGES), applyChanges);
+
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTAPPLICATIONGROUP_TRUSTLIST_ADDCERTIFICATE), addCertificate);
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_TRUSTLISTTYPE_ADDCERTIFICATE), addCertificate);
+
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTUSERTOKENGROUP_TRUSTLIST_ADDCERTIFICATE), addCertificate);
+
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTAPPLICATIONGROUP_TRUSTLIST_REMOVECERTIFICATE), removeCertificate);
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_TRUSTLISTTYPE_REMOVECERTIFICATE), removeCertificate);
+
+    retval |= setMethodNode_callback(server, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVERCONFIGURATION_CERTIFICATEGROUPS_DEFAULTUSERTOKENGROUP_TRUSTLIST_ADDCERTIFICATE), removeCertificate);
 
     return retval;
 }
