3fd67b89affe5a3c837a9795b998c7c7132f7e37
Noel Graf <noel.gerome.graf@gmail.com>
2025-05-23 15:40:34 +0200

    fix

--- tools/ci.sh
+++ tools/ci.sh
@@ -424,7 +424,7 @@ function build_clang_analyzer {
           --status-bugs \
           --exclude ../src/util \
           --exclude ../tests \
-          --exclude ../deps/nodesetLoader \
+          --exclude '.*deps/nodesetLoader.*' \
           make ${MAKEOPTS}
 }
 
